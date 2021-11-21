1a2dfd6b3b4689a67da4c215dbd9ced7c0562bea
Salsabila1105 <75847941+Salsabila1105@users.noreply.github.com>
2021-11-21 07:39:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+#membuat class
+class televisi:
+  def __init__(self,merk,channel,remote,volume): 
+    self.merk = merk
+    self.channel = channel
+    self.remote = remote
+    self.__volume = None
+
+  def turontelevisi(self):
+    print('televisi ON',  self.remote,)
+  def turnofftelevisi(self):
+    print('televisi OFF',self.remote)
+  def setvolume(self, num):
+    #private
+    self.__volume = num
+    print('volume = ', self.__volume)
+
+  def getVolume(self):
+    return self.__volume
+
+  def gantichannel(self):
+    print('Televisi channel :', self.channel)
+
+
+#membuat childclass
+#overriding
+class Television(televisi):
+  def __init__(self,merk,channel,remote,volume):
+    super(Television, self).__init__(merk,channel,remote,volume)
+  def gantichannel(self):
+    print('Televisi berganti ke channel :', self.channel)
+
+
+TV1 = televisi('Polytron', 45, 'Mode', 100)
+TV2 = Television('LG', 65, 'Mode', 100)
+
+
+print(TV1)
+
+#atribut ke objek
+TV1.turontelevisi()
+TV2.turnofftelevisi()
+TV1.setvolume(100)
+TV2.gantichannel()
+print(TV1.getVolume())
+print(TV1.merk)
+
+
+   
